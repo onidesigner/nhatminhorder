23b6065be1993a20d1f7663621e1af5689c2ac3a
vanhs <nhatminh14071990@gmail.com>
2017-04-04 14:39:26 +0700

    revert file

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -36,8 +36,8 @@
                                         <th>Đối tượng</th>
                                         <th>Họ & tên</th>
                                         <th>Trạng thái</th>
-                                        <th>Email</th>
                                         <th>Thời gian</th>
+                                        <th>Số dư cuối</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -48,18 +48,19 @@
                                             <td>{{$user->id}}</td>
                                             <td>{{ App\User::getSectionName($user->section)  }}</td>
                                             <td>
-
-
                                                 <a href="{{ url('user/detail', $user->id)  }}">{{$user->name}}</a>
 
                                                 (<code>{{$user->code}}</code>)
+
+                                                <br>
+                                                <small>{{$user->email}}</small>
                                             </td>
                                             <td>{{ App\User::getStatusName($user->status)  }}</td>
                                             <td>
-                                                {{$user->email}}
-                                            </td>
-                                            <td>
                                                 Gia nhập: {{ date('H:i d/m/Y', strtotime($user->created_at)) }}
+                                            </td>
+                                            <td class="text-right">
+                                                {{ App\Util::formatNumber($user->account_balance)  }} <sup>đ</sup>
                                             </td>
                                         </tr>
                                     @endforeach
