540d2a3992d326ed7a5a98e63d4ea2ce50b7ce9c
vanhs <nhatminh14071990@gmail.com>
2017-04-12 12:41:37 +0700

    code phan in tem kien

--- resources/views/logistic_package_barcode_print.blade.php
+++ resources/views/logistic_package_barcode_print.blade.php
@@ -13,13 +13,13 @@
     <span style='font-weight: bold;
                 font-family: Helvetica Neue, Helvetica, Arial, San-Serif;
                 text-align: right;'>
-        {{$package->logistic_package_barcode}}
+        &nbsp;{{$package->logistic_package_barcode}}
     </span>
     <br>
 
-    {!!$svg!!}
+    &nbsp;{!!$svg!!}
     {{--<img src="data:image/png;base64,{{$img_base_64}}">--}}
-    <br><span>@if($package->isTransportStraight()) CT @endif @if($package->getOrder()) {{$package->getOrder()->destination_warehouse}} @endif {{$package->getWeightCalFee()}}kg</span>
+    <br><span>&nbsp;@if($package->isTransportStraight())CT @endif @if($package->getOrder()){{$package->getOrder()->destination_warehouse}} @endif {{$package->getWeightCalFee()}}kg</span>
 </div>
 
 <script>
