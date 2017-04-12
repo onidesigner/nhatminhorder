50d9ce9508447e4995abd23ceb4d06543b68f77c
vanhs <nhatminh14071990@gmail.com>
2017-04-12 12:56:19 +0700

    code phan in tem kien

--- resources/views/logistic_package_barcode_print.blade.php
+++ resources/views/logistic_package_barcode_print.blade.php
@@ -12,20 +12,21 @@
 <div class="content">
     <span style='
                 font-family: Helvetica Neue, Helvetica, Arial, San-Serif;
-                text-align: right;
-                font-weight: 500; display: inline-block; text-align: left; width: 100%;'>
-        &nbsp;{{$package->logistic_package_barcode}}
+                font-weight: 500; display: inline-block; text-align: left; width: 100%; padding: 0 5px;'>
+        {{$package->logistic_package_barcode}}
     </span>
     <br>
 
-    <span style=" display: inline-block; text-align: left; width: 100%;">
-    &nbsp;{!!$svg!!}
+    <span style=" display: inline-block; text-align: left; width: 50%; padding: 0 5px;">
+    {!!$svg!!}
     </span>
+    
     {{--<img src="data:image/png;base64,{{$img_base_64}}">--}}
     <br>
-    <span style="font-size: 11px; display: inline-block; text-align: left; width: 100%;">@if($package->isTransportStraight())
-            &nbsp;CT&nbsp;@endif
-        @if($package->getOrder())&nbsp;{{$package->getOrder()->destination_warehouse}}&nbsp;@endif
+    <span style="font-size: 11px; display: inline-block; text-align: left; width: 100%; padding: 0 5px;">
+        @if($package->isTransportStraight())
+            CT&nbsp;@endif
+        @if($package->getOrder()){{$package->getOrder()->destination_warehouse}}&nbsp;@endif
         {{$package->getWeightCalFee()}}kg
     </span>
 </div>
