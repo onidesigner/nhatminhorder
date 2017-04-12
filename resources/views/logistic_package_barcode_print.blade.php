3ce2b9d9aef1f106d1197d8281e85fb8917f00d4
vanhs <nhatminh14071990@gmail.com>
2017-04-12 13:34:23 +0700

    code phan in tem kien

--- resources/views/logistic_package_barcode_print.blade.php
+++ resources/views/logistic_package_barcode_print.blade.php
@@ -13,7 +13,7 @@
     }
 </style>
 
-<div class="content">
+<div class="content" style="display: inline-block; width: 40%; float: left; margin-right: 5%;">
     <div style='
                 font-family: Helvetica Neue, Helvetica, Arial, San-Serif;
                 font-weight: 500; display: inline-block; text-align: left; width: 100%; padding: 0 5px;'>
@@ -21,7 +21,7 @@
     </div>
 
     <div style=" display: inline-block; text-align: left; width: 50%; padding: 0 5px;">
-    {!!$svg!!}
+    {{--{!!$svg!!}--}}
     </div>
     
     <div style="font-size: 11px; display: inline-block; text-align: left; width: 100%; padding: 0 5px;">
@@ -30,6 +30,25 @@
         <span class=""> {{$package->getWeightCalFee()}}kg </span>
     </div>
 </div>
+
+<div class="content" style="display: inline-block; width: 40%; float: left; margin-left: 5%">
+    <div style='
+                font-family: Helvetica Neue, Helvetica, Arial, San-Serif;
+                font-weight: 500; display: inline-block; text-align: left; width: 100%; padding: 0 5px;'>
+        {{$package->logistic_package_barcode}}
+    </div>
+
+    <div style=" display: inline-block; text-align: left; width: 50%; padding: 0 5px;">
+        {{--{!!$svg!!}--}}
+    </div>
+
+    <div style="font-size: 11px; display: inline-block; text-align: left; width: 100%; padding: 0 5px;">
+        @if($package->isTransportStraight()) CT @endif
+        @if($package->getOrder()) {{$package->getOrder()->destination_warehouse}} @endif
+        <span class=""> {{$package->getWeightCalFee()}}kg </span>
+    </div>
+</div>
+
 
 <script>
     window.print();
