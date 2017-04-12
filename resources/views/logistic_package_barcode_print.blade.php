bceeae055da908864c782719a43be9b250e99386
vanhs <nhatminh14071990@gmail.com>
2017-04-12 12:53:54 +0700

    code phan in tem kien

--- resources/views/logistic_package_barcode_print.blade.php
+++ resources/views/logistic_package_barcode_print.blade.php
@@ -1,7 +1,7 @@
 <style>
     @media print {
         @page {
-            size: 70mm 20mm; /* landscape */
+            size: 70mm 22mm; /* landscape */
             /* you can also specify margins here: */
             font-size: 80px;
             margin: 0;
@@ -13,15 +13,17 @@
     <span style='
                 font-family: Helvetica Neue, Helvetica, Arial, San-Serif;
                 text-align: right;
-                font-weight: 500;'>
+                font-weight: 500; display: inline-block; text-align: left; width: 100%;'>
         &nbsp;{{$package->logistic_package_barcode}}
     </span>
     <br>
 
+    <span style=" display: inline-block; text-align: left; width: 100%;">
     &nbsp;{!!$svg!!}
+    </span>
     {{--<img src="data:image/png;base64,{{$img_base_64}}">--}}
     <br>
-    <span>@if($package->isTransportStraight())
+    <span style="font-size: 11px; display: inline-block; text-align: left; width: 100%;">@if($package->isTransportStraight())
             &nbsp;CT&nbsp;@endif
         @if($package->getOrder())&nbsp;{{$package->getOrder()->destination_warehouse}}&nbsp;@endif
         {{$package->getWeightCalFee()}}kg
