b60f830624fddf43b1e5331b63d992f6c2b52ee0
vanhs <nhatminh14071990@gmail.com>
2017-04-12 13:21:11 +0700

    code phan in tem kien

--- resources/views/logistic_package_barcode_print.blade.php
+++ resources/views/logistic_package_barcode_print.blade.php
@@ -25,11 +25,9 @@
     </div>
     
     <div style="font-size: 11px; display: inline-block; text-align: left; width: 100%; padding: 0 5px;">
-        @if($package->isTransportStraight())
-            CT
-        @endif
-        @if($package->getOrder()){{$package->getOrder()->destination_warehouse}} @endif
-        <span class="padding-left-5">{{$package->getWeightCalFee()}}kg</span>
+        @if($package->isTransportStraight()) CT @endif
+        @if($package->getOrder()) {{$package->getOrder()->destination_warehouse}} @endif
+        <span class=""> {{$package->getWeightCalFee()}}kg </span>
     </div>
 </div>
 
