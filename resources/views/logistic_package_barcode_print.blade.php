1d3c30b6ba782efdc9da680c5f0e00eb7d0a88b1
vanhs <nhatminh14071990@gmail.com>
2017-04-12 13:19:45 +0700

    code phan in tem kien

--- resources/views/logistic_package_barcode_print.blade.php
+++ resources/views/logistic_package_barcode_print.blade.php
@@ -3,33 +3,34 @@
         @page {
             size: 70mm 22mm; /* landscape */
             /* you can also specify margins here: */
-            font-size: 80px;
             margin: 0;
+            padding: 0;
         }
+    }
+
+    .padding-left-5{
+        padding-left: 5px;
     }
 </style>
 
 <div class="content">
-    <span style='
+    <div style='
                 font-family: Helvetica Neue, Helvetica, Arial, San-Serif;
                 font-weight: 500; display: inline-block; text-align: left; width: 100%; padding: 0 5px;'>
         {{$package->logistic_package_barcode}}
-    </span>
-    <br>
+    </div>
 
-    <span style=" display: inline-block; text-align: left; width: 50%; padding: 0 5px;">
+    <div style=" display: inline-block; text-align: left; width: 50%; padding: 0 5px;">
     {!!$svg!!}
-    </span>
+    </div>
     
-    {{--<img src="data:image/png;base64,{{$img_base_64}}">--}}
-    <br>
-    <span style="font-size: 11px; display: inline-block; text-align: left; width: 100%; padding: 0 5px;">
+    <div style="font-size: 11px; display: inline-block; text-align: left; width: 100%; padding: 0 5px;">
         @if($package->isTransportStraight())
             CT
         @endif
         @if($package->getOrder()){{$package->getOrder()->destination_warehouse}} @endif
-        <span style="padding-left: 5px;">{{$package->getWeightCalFee()}}kg</span>
-    </span>
+        <span class="padding-left-5">{{$package->getWeightCalFee()}}kg</span>
+    </div>
 </div>
 
 <script>
