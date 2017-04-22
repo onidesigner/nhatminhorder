67653910e1a9e331c73d371f6cf82a174a8e5093
vanhs <nhatminh14071990@gmail.com>
2017-04-22 15:15:57 +0700

    code phan in tem kien

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -19,151 +19,56 @@
                 <div class="card-body">
 
                     <div class="row">
-                        <div class="col-sm-12 col-xs-12">
-
-                            <h3 class="cart-title">Quét mã vạch</h3>
-
-                            <div class="stepwizard">
-                                <div class="stepwizard-row">
-                                    <div class="stepwizard-step" style="width: 20%">
-                                        <button type="button" class="btn btn-default btn-circle">1</button>
-                                        <p>Chọn kho</p>
-                                    </div>
-                                    <div class="stepwizard-step" style="width: 20%">
-                                        <button type="button" class="btn btn-primary btn-circle">2</button>
-                                        <p>Chọn hành động</p>
-                                    </div>
-                                    <div class="stepwizard-step" style="width: 60%">
-                                        <button type="button" class="btn btn-primary btn-circle">3</button>
-                                        <p>Quét mã</p>
-                                    </div>
-
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-sm-3 col-xs-12">
-                                        <h4>Kho nhận Trung Quốc</h4>
-
-                                        @if(!empty($warehouse_list))
-                                            @foreach($warehouse_list as $key => $val)
-
-                                                @if($val['type'] == \App\WareHouse::TYPE_RECEIVE)
-                                                    <p style="padding-left: 30px;">
+                        <div class="col-sm-4 col-xs-12">
+                            <h3>Quét mã vạch</h3>
 
 
-                                                        <input name="warehouse" type="radio" value="{{$val['code']}}" id="{{$val['code']}}"> <label for="{{$val['code']}}">{{$val['name']}} ({{$val['code']}})</label>
-                                                    </p>
-                                                @endif
+                            <form onsubmit="return false;" class="___form" id="_from-scan-barcode">
 
-
-
-                                                {{--<option--}}
-                                                {{--data-warehouse-type="{{$val['type']}}"--}}
-                                                {{--value="{{$val['code']}}">{{$val['name']}} - {{$val['description']}}</option>--}}
-                                            @endforeach
-                                        @endif
-
-                                    <h4>Kho phân phối Việt Nam</h4>
-
-
-                                    @if(!empty($warehouse_list))
-                                        @foreach($warehouse_list as $key => $val)
-
-                                            @if($val['type'] == \App\WareHouse::TYPE_DISTRIBUTION)
-                                                <p style="padding-left: 30px;">
-                                                    <input name="warehouse"
-                                                           type="radio"
-                                                           value="{{$val['code']}}"
-                                                           id="{{$val['code']}}">
-                                                    <label for="{{$val['code']}}">{{$val['name']}} ({{$val['code']}})</label>
-                                                </p>
-                                            @endif
-
-                                            {{--<option--}}
-                                            {{--data-warehouse-type="{{$val['type']}}"--}}
-                                            {{--value="{{$val['code']}}">{{$val['name']}} - {{$val['description']}}</option>--}}
-                                        @endforeach
-                                    @endif
-                                </div>
-                                <div class="col-sm-3 col-xs-12">
-
-                                    <h4>&nbsp;</h4>
-
+                                <select name="action" id="" class="form-control">
                                     @if(!empty($action_list))
                                         @foreach($action_list as $key => $val)
-                                            <p>
-                                                <input name="action"
-                                                       type="radio"
-                                                       id="{{$key}}"
-                                                       value="{{$key}}">
-                                                <label for="{{$key}}">{{$val}}</label>
-                                            </p>
-
+                                            <option value="{{$key}}">
+                                                {{$val}}
+                                            </option>
                                         @endforeach
                                     @endif
+                                </select>
+                                <br>
 
+                                <select name="warehouse" id="" class="form-control">
+                                    @if(!empty($warehouse_list))
+                                        @foreach($warehouse_list as $key => $val)
+                                            <option
+                                                    data-warehouse-type="{{$val['type']}}"
+                                                    value="{{$val['code']}}">{{$val['name']}} - {{$val['description']}}</option>
+                                        @endforeach
+                                    @endif
+                                </select>
+                                <br>
 
-                                </div>
-                                <div class="col-sm-3 col-xs-12">
-                                    <input
+                                <input type="hidden" name="method" value="post">
+                                <input type="hidden" name="url" value="{{ url('scan/action') }}">
+                                <input type="hidden" name="_token" value="{{ csrf_token()  }}">
 
-                                            id="_scan-logistic-package-barcode"
-                                            autofocus
-                                            type="text"
-                                            name="barcode"
-                                            class="form-control _scan-barcode scan-barcode"
-                                            data-key-global="barcode-scan-input"
-                                            placeholder="Mã kiện">
-                                </div>
+                                <input
+                                        id="_scan-logistic-package-barcode"
+                                        autofocus
+                                        type="text"
+                                        name="barcode"
+                                        class="form-control _scan-barcode"
+                                        data-key-global="barcode-scan-input"
+                                        placeholder="Quét mã kiện">
 
-                            </div>
-
-
-
-
-                            {{--<form onsubmit="return false;" class="___form" id="_from-scan-barcode">--}}
-
-                                {{--<select name="action" id="" class="form-control">--}}
-                                    {{--@if(!empty($action_list))--}}
-                                        {{--@foreach($action_list as $key => $val)--}}
-                                            {{--<option value="{{$key}}">--}}
-                                                {{--{{$val}}--}}
-                                            {{--</option>--}}
-                                        {{--@endforeach--}}
-                                    {{--@endif--}}
-                                {{--</select>--}}
-                                {{--<br>--}}
-
-                                {{--<select name="warehouse" id="" class="form-control">--}}
-                                    {{--@if(!empty($warehouse_list))--}}
-                                        {{--@foreach($warehouse_list as $key => $val)--}}
-                                            {{--<option--}}
-                                                    {{--data-warehouse-type="{{$val['type']}}"--}}
-                                                    {{--value="{{$val['code']}}">{{$val['name']}} - {{$val['description']}}</option>--}}
-                                        {{--@endforeach--}}
-                                    {{--@endif--}}
-                                {{--</select>--}}
-                                {{--<br>--}}
-
-                                {{--<input type="hidden" name="method" value="post">--}}
-                                {{--<input type="hidden" name="url" value="{{ url('scan/action') }}">--}}
-                                {{--<input type="hidden" name="_token" value="{{ csrf_token()  }}">--}}
-
-                                {{--<input--}}
-                                        {{--id="_scan-logistic-package-barcode"--}}
-                                        {{--autofocus--}}
-                                        {{--type="text"--}}
-                                        {{--name="barcode"--}}
-                                        {{--class="form-control _scan-barcode"--}}
-                                        {{--data-key-global="barcode-scan-input"--}}
-                                        {{--placeholder="Quét mã kiện">--}}
-
-                            {{--</form>--}}
+                            </form>
 
 
                         </div>
+                        <div class="col-sm-8 col-xs-12">
+                            {{--<h3>Kết quả quét</h3>--}}
 
+
+                        </div>
                     </div>
 
                 </div>
@@ -171,28 +76,6 @@
         </div>
     </div>
 
-@endsection
-
-@section('css_bottom')
-    @parent
-    <style>
-        .scan-barcode{
-            border-left: 0!important;
-            border-right: 0!important;;
-            border-top: 0!important;;
-            border-bottom: 2px solid #29c75f!important;;
-        }
-        .scan-barcode:focus{
-            box-shadow: none!important;
-        }
-
-        .stepwizard-step{
-            /*width: 25%;*/
-        }
-        .well{
-            padding: 0 19px;
-        }
-    </style>
 @endsection
 
 @section('js_bottom')
