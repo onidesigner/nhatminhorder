740f1f10259bc24d932430a1bcf020dcdc701d4f
goerge <nguyenhoanggiang@aliamama.vn>
2017-06-08 16:38:05 +0700

    send sms to customer

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -22,84 +22,7 @@ class ReportController extends Controller
     {
         $this->middleware('auth');
     }
-
-    /**
-     * Ham goi den danh sach
-     */
-    public function indexs(){
-
-        // đẩy dữ liệu từ trên server xuống dưới
-        $data_package = Package::where('is_deleted',0)
-
-            ->whereIn('status',[Package::STATUS_RECEIVED,Package::STATUS_DELIVERING])->get();
-
-        $data_package_paginate = Package::where('is_deleted',0)
-
-            ->whereIn('status',[Package::STATUS_RECEIVED,Package::STATUS_DELIVERING])->get();
-
-        $data_return = [];
-
-        $package_weight = 0;
-        if(sizeof($data_package) > 0){
-
-            foreach ($data_package as $item_package){
-                /** @var Package $item_package */
-                if($item_package->weight == 0 && $item_package->convert_weight == 0){
-                        continue;
-                }
-                $package_weight += $item_package->getWeightCalFee();
-
-            }
-            /**
-             * laays gas trij hienj thi
-             */
-            foreach ($data_package_paginate as $item_package_panigate){
-                if($item_package_panigate->weight == 0 && $item_package_panigate->convert_weight == 0){
-                    continue;
-                }
-                $data_return[] = $item_package_panigate;
-            }
-        }
-
-
-        // thống kê tổng giá trị tiền vận chuyển nội địa chung quốc
-
-        # tống tiền phí dịch vụ
-        $buying_fees = OrderFee::where('name','BUYING_FEE_VND')->get();
-
-        $total_buying_fee = 0;
-        foreach ($buying_fees as $item_buying){
-            /** @var $item_buying OrderFee */
-            if(self::checkStatusOrder($item_buying->order_id) == true){
-                $total_buying_fee += $item_buying->money;
-            }
-        }
-        #end tổng phí dịch vụ
-
-        #region phí vận chuyển nội địa chung quốc
-
-        $domictic_shipping_fee = OrderFee::where('name','DOMESTIC_SHIPPING_FEE_VND')->get();
-        $total_domictic_shipping_fee = 0;
-        foreach ($domictic_shipping_fee as $item_domictic){
-            /** @var $item_domictic OrderFee */
-            $total_domictic_shipping_fee += $item_domictic->money;
-        }
-
-        #endregion Phí vận chuyển nội địa trung quốc
-
-
-
-
-        return view('report',[
-            'data' => $data_return,
-            'total_package' => $package_weight,
-            'total_buying_fee' => $total_buying_fee,
-            'total_domictic_shipping_fee' => $total_domictic_shipping_fee,
-            'page_title' => 'Báo cáo sản lượng hàng tháng',
-        ]);
-
-    }
-
+    
     /**
      * kiểm tra trạng thái của đơn hàng
      * @param $order_id
@@ -154,7 +77,7 @@ class ReportController extends Controller
             }
             
         }
-        
+
         $packages = $packages->get();
         $total_package = $packages->count(); // tong so luong kien hang
 
@@ -170,6 +93,7 @@ class ReportController extends Controller
         }
 
         $list_order_id = array_unique($order_id);
+
 
 
         /**
@@ -206,22 +130,13 @@ class ReportController extends Controller
 
         }
         return view('report',[
-
+            'package_weight' =>$package_weight ,
             'total_package' => $total_package,
+            'customer_payment_order' => $customer_payment_order,
             'total_buying_fee' => $buying_fee,
             'total_domictic_shipping_fee' => $domestic_shipping_fee,
             'page_title' => 'Báo cáo sản lượng hàng tháng',
         ]);
-
-
-
-
-
-
-
-
-
-
 
     }
 
--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -4,111 +4,162 @@
     {{$page_title}}
 @endsection
 @section('content')
-    <form action="/san-luong-van-chuyen" method="GET">
-        <div class="row" class="col-sm-12" >
-            <p>
-                <div class="col-sm-3">
-                    <span>
 
-                        <?php
 
-                            $date_from = empty(request()->get('date1')) ? date('Y-m-d') : request()->get('date1');
-                            $date_to = empty(request()->get('date2')) ? date('Y-m-d') : request()->get('date2');
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
 
-                        ?>
+                @include('partials/__breadcrumb',
+                                [
+                                    'urls' => [
+                                        ['name' => 'Trang chủ', 'link' => url('home')],
+                                        ['name' => 'Thống kê', 'link' => null],
+                                    ]
+                                ]
+                            )
 
-                        {{ Form::text('date1', $date_from, array('id' => 'datepicker') )}}
-                    </span>
-                </div>
+                <div class="card-body">
 
-                <div class="col-sm-3">
-                    <span>
-                        {{ Form::text('date2', $date_to, array('id' => 'datepicker2') )}}
-                    </span>
-                </div>
+                    <form action="/san-luong-van-chuyen" method="GET">
+                        <div class="row" class="col-sm-12">
 
-                <div class="col-sm-3">
-                    <span>
-                            <div class="form-group">
-                              <select class="form-control" name="warehouse">
-                                  <option value="0">Chọn kho</option>
-                                  <option value="CNGZ">CNGZ - Kho Quảng Châu</option>
-                                  <option value="CNPX">CNPX - Bằng Tường</option>
-                                  <option value="K-HN">K-HN - Hà Nội</option>
-                                  <option value="S-SG">S-SG - Sài Gòn</option>
-                              </select>
+
+
+                            <div class="col-sm-2">
+                                <span>
+                                    <?php
+                                        $warehouse = request()->get('warehouse');
+                                    ?>
+                                    <div class="form-group">
+                                          <select class="form-control" name="warehouse">
+                                              <option value="0">Chọn kho</option>
+                                              <option value="CNGZ"
+                                              <?php
+                                                      if ($warehouse == 'CNGZ') {
+                                                          echo "selected";
+                                                      }
+                                                      ?>
+                                              >CNGZ - Kho Quảng Châu</option>
+                                              <option value="CNPX"
+                                              <?php
+                                                      if ($warehouse == 'CNPX') {
+                                                          echo "selected";
+                                                      }
+                                                      ?>
+
+                                              >CNPX - Bằng Tường</option>
+                                              <option value="K-HN"
+                                              <?php
+                                                      if ($warehouse == 'K-HN') {
+                                                          echo "selected";
+                                                      }
+                                                      ?>
+
+                                              >K-HN - Hà Nội</option>
+                                              <option value="S-SG"
+                                              <?php
+                                                      if ($warehouse == 'S-SG') {
+                                                          echo "selected";
+                                                      }
+                                                      ?>
+
+                                              >S-SG - Sài Gòn</option>
+                                          </select>
+                                        </div>
+                                </span>
                             </div>
-                    </span>
-                </div>
 
-                <div class="col-sm-3">
-                    <span>
-                            <div class="form-group">
-                              <select class="form-control" name="warehouse_status">
-                                <option value="0">Chọn trạng thái</option>
-                                <option value="IN">Nhập</option>
-                                <option value="OUT">Xuất</option>
-                              </select>
+                            <div class="col-sm-2">
+                                <span>
+                                    <?php
+                                    $warehouse_status = request()->get('warehouse_status');
+                                    ?>
+                                    <div class="form-group">
+                                          <select class="form-control" name="warehouse_status">
+                                            <option value="0">Trạng thái</option>
+                                            <option value="IN"
+                                            <?php
+                                                    if ($warehouse_status == 'IN') {
+                                                        echo "selected";
+                                                    }
+                                                    ?>
+
+
+                                            >Nhập</option>
+                                            <option value="OUT"
+
+                                            <?php
+                                                    if ($warehouse_status == 'OUT') {
+                                                        echo "selected";
+                                                    }
+                                                    ?>
+
+                                            >Xuất</option>
+                                          </select>
+                                        </div>
+                                </span>
                             </div>
-                    </span>
-                </div>
 
-                <div>
-                        <span>
-                            <button  type="submit" class="btn btn-primary"
-                             style="padding: 0px !important;"
-                            >Tìm Kiếm</button>
-                            </span>
-                </div>
 
-            </p>
+                            <div class="col-sm-3">
+                                    <span>
+
+                                        <?php
+
+                                        $date_from = empty(request()->get('date1')) ? date('Y-m-d') : request()->get('date1');
+                                        $date_to = empty(request()->get('date2')) ? date('Y-m-d') : request()->get('date2');
+
+                                        ?>
+
+                                        Từ: {{ Form::text('date1', $date_from, array('id' => 'datepicker') )}}
+                                    </span>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <span>
+                                 Đến: {{ Form::text('date2', $date_to, array('id' => 'datepicker2') )}}
+                                </span>
+                            </div>
+
+                            <div>
+                                <span>
+                                    <button type="submit" class="btn btn-primary"
+
+                                    >Tìm Kiếm</button>
+                                </span>
+                            </div>
+
+                        </div>
+                        <p>
+                            <span>Tổng số kiện hàng <strong>{{ $total_package }} kiện</strong></span>
+                        </p>
+                        <p>
+                            <span>Tổng sản lượng Cân nặng <strong>{{ $package_weight }} kg</strong></span>
+                        </p>
+                        <p>
+                            <span>Tổng phí mua hàng ( 1% ) <strong>{{ App\Util::formatNumber($total_buying_fee) }}
+                                    VND</strong></span>
+                        </p>
+                        <p>
+                            <span>Tổng tiền vận chuyển nội địa Trung Quốc <strong>{{ App\Util::formatNumber($total_domictic_shipping_fee)}}
+                                    VND</strong> </span>
+                        </p>
+                        <p>
+                            <span>Tổng tiền hàng <strong>{{App\Util::formatNumber($customer_payment_order)}}
+                                    VND</strong></span>
+                        </p>
+
+
+                    </form>
+
+
+                </div>
+            </div>
         </div>
-        <p>
-            <span>Tổng sản lượng vận chuyển <strong>{{ $total_package }} kg</strong></span>
-        </p>
-        <p>
-            <span>Tổng phí mua hàng ( 1% ) <strong>{{ $total_buying_fee }} VND</strong></span>
-        </p>
-        <p>
-            <span>Tổng tiền vận chuyển nội địa Trung Quốc <strong>{{$total_domictic_shipping_fee}} VND</strong> </span>
-        </p>
-
-        <p>
-            <span>Tổng tiền vận chuyển Quốc tế (Trung - Việt) <strong>{{$total_domictic_shipping_fee}} VND</strong> </span>
-        </p>
-        <p>
-            <span>Tổng tiền vận chuyển Nội địa việt nam <strong>{{$total_domictic_shipping_fee}} VND</strong> </span>
-        </p>
+    </div>
 
 
-   {{-- <table class="table">
-        <thead class="thead-default">
-        <tr>
-            <th>Khách Hàng</th>
-            <th>Mã Kiện</th>
-            <th>Mã đơn</th>
-            <th>Cân nặng (kg)</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($data as $item)
-            <tr>
-                <td>
-                    {{ \App\Util::getUserName($item->buyer_id) }}
-
-                </td>
-                <td> {{ $item->logistic_package_barcode }} </td>
-                <td>
-                    {{ \App\Order::findOneByIdOrCode($item->order_id)->code }}
-                </td>
-                <td>
-                    {{ \App\Util::getWeightFee($item) }}
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>--}}
-    </form>
 @endsection
 
 @section('js_bottom')
@@ -116,9 +167,9 @@
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>
     <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
     <script>
-        $(function() {
-            $( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd' });
-            $( "#datepicker2" ).datepicker({ dateFormat: 'yy-mm-dd' });
+        $(function () {
+            $("#datepicker").datepicker({dateFormat: 'yy-mm-dd'});
+            $("#datepicker2").datepicker({dateFormat: 'yy-mm-dd'});
         });
     </script>
 
