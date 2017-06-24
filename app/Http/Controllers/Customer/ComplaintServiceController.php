63c8758a8af592f1f7d7242f6252ef89cfced466
goerge <nguyenhoanggiang@aliamama.vn>
2017-06-24 15:09:57 +0700

    update khieu nai

--- app/Http/Controllers/Customer/ComplaintServiceController.php
+++ app/Http/Controllers/Customer/ComplaintServiceController.php
@@ -75,6 +75,7 @@ class ComplaintServiceController extends Controller
      */
     public function createComplaint()
     {
+
         $img = $_FILES['image'];
         $order_code = $_POST['order_code'];
         $title = $_POST['title_complaint'];
@@ -149,6 +150,21 @@ class ComplaintServiceController extends Controller
         // nếu tạo thành công chuyển về trang chi tiết khiếu nại
         return redirect('chi-tiet-khieu-nai/'.$complaint_id)->with('message','Tạo khiệu nại thành công !');
 
+    }
+
+    /**
+     * hàm thực hiện upload ảnh mà ko cần click submit
+     */
+    public function uploadImageInComplaint(){
+        if($_FILES["file"]["name"] != '')
+        {
+            $test = explode('.', $_FILES["file"]["name"]);
+            $ext = end($test);
+            $name = rand(100, 999) . '.' . $ext;
+            $location = './upload/' . $name;
+            move_uploaded_file($_FILES["file"]["tmp_name"], $location);
+            echo '<img src="'.$location.'" height="150" width="225" class="img-thumbnail" />';
+        }
     }
 
     /**
--- resources/views/customer/complaint_create.blade.php
+++ resources/views/customer/complaint_create.blade.php
@@ -54,7 +54,10 @@
                     <label class="col-md-4 control-label">Upload ảnh</label>
                  <div class="col-md-4 inputGroupContainer">
                         <div class="input-group" style=" border: none;!important">
-                            <input type="file" name="image[]" multiple/>
+                            {{--<input type="file" name="image[]" multiple/>--}}
+                            <input type="file" name="file" id="file" />
+                            <br>
+                            <span id="uploaded_image"></span>
                         </div>
                     </div>
                 </div>
@@ -91,4 +94,46 @@
 @section('js_bottom')
     @parent
     <script type="text/javascript" src="{{ asset('js/jquery.lazy.min.js') }}"></script>
+    <script>
+        $(document).ready(function(){
+            $(document).on('change', '#file', function(){
+                var name = document.getElementById("file").files[0].name;
+                var form_data = new FormData();
+                var ext = name.split('.').pop().toLowerCase();
+                if(jQuery.inArray(ext, ['gif','png','jpg','jpeg']) == -1)
+                {
+                    alert("Invalid Image File");
+                }
+                var oFReader = new FileReader();
+                oFReader.readAsDataURL(document.getElementById("file").files[0]);
+                var f = document.getElementById("file").files[0];
+                var fsize = f.size||f.fileSize;
+                if(fsize > 2000000)
+                {
+                    alert("Image File Size is very big");
+                }
+                else
+                {
+                    form_data.append("file", document.getElementById('file').files[0]);
+                    $.ajax({
+                        url: '',
+                        method:"POST",
+                        data: form_data,
+                        contentType: false,
+                        cache: false,
+                        processData: false,
+                        beforeSend:function(){
+                            $('#uploaded_image').html("<label class='text-success'>Image Uploading...</label>");
+                        },
+                        success:function(data)
+                        {
+                            $('#uploaded_image').html(data);
+                        }
+                    });
+                }
+            });
+        });
+    </script>
+
+
 @endsection
